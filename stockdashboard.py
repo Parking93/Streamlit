4260fbe4a71ae9885754acf3892be10633ba2359
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-06-16 08:57:59 +0900

    Update stockdashboard.py

--- stockdashboard.py
+++ stockdashboard.py
@@ -35,18 +35,18 @@ def main():
     end_date_str = end_date.strftime('%Y-%m-%d')
     
     
-    # 데이터 생성
-    df = fdr.DataReader(stock[0], start_date_str, end_date_str)
-    st.dataframe(df)
+    # # 데이터 생성
+    # df = fdr.DataReader(stock[0], start_date_str, end_date_str)
+    # st.dataframe(df)
     
-    # 라인 그래프 생성
-    st.line_chart(df)
+    # # 라인 그래프 생성
+    # st.line_chart(df)
 
-    # 막대 그래프 생성
-    st.bar_chart(df)
+    # # 막대 그래프 생성
+    # st.bar_chart(df)
     # 라인 그래프 생성 with for문
     for i in range(len(stock)):
-        st.line_chart(stock[i], start_date_str, end_date_str)
+        st.line_chart(fdr.DataReader(stock[i], start_date_str, end_date_str))
 
 
 if __name__ == "__main__":
