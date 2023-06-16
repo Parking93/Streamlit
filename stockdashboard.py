306ce3219dd9804dda5d6e8770779f9771083093
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-06-17 00:33:18 +0900

    Update stockdashboard.py

--- stockdashboard.py
+++ stockdashboard.py
@@ -38,13 +38,16 @@ def main():
     start_date_str = start_date.strftime('%Y-%m-%d')
     end_date_str = end_date.strftime('%Y-%m-%d')
     
-    
+    # 매트릭 생성 
+    for i in range(len(stock_list)):
+        st.metric(label=f'{stock[i]}', value=f'{fdr.DataReader(stock_list[i], start_date_str, end_date_str)['Close'].loc[end_date_str]}',
+                  delta=f'{fdr.DataReader(stock_list[i], start_date_str, end_date_str)['Close'].loc[end_date_str]} - fdr.DataReader(stock_list[i], start_date_str, end_date_str)['Close'].loc[start_list[i]]}}'
 
     # 막대 그래프 생성
     st.subheader('📊막대 그래프')
     for i in range(len(stock_list)):
         st.subheader(f'{stock[i]}')
-        st.bar_chart(fdr.DataReader(stock_list[i], start_date_str, end_date_str)[['Close','Open']])
+        st.bar_chart(fdr.DataReader(stock_list[i], start_date_str, end_date_str)['Close'])
     
     # 라인 그래프 생성 with for문
     st.subheader('📈라인 그래프')
