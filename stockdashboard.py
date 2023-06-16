e9133e2df6850f391b811efa3160f8c25861405c
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-06-17 00:39:17 +0900

    Update stockdashboard.py

--- stockdashboard.py
+++ stockdashboard.py
@@ -40,7 +40,8 @@ def main():
     
     # 매트릭 생성 
     for i in range(len(stock_list)):
-        st.metric(label=f'{stock[i]}', value=f'{fdr.DataReader(stock_list[i], start_date_str, end_date_str)["Close"].loc[end_date_str]}')
+        st.metric(label=f'{stock[i]}', value=f'{fdr.DataReader(stock_list[i], start_date_str, end_date_str)["Close"].loc[end_date_str]}원',
+                 delta=f'{fdr.DataReader(stock_list[i], start_date_str, end_date_str)["Close"].loc[end_date_str]-fdr.DataReader(stock_list[i], start_date_str, end_date_str)["Close"].loc[start_date_str]}원')
 
     # 막대 그래프 생성
     st.subheader('📊막대 그래프')
