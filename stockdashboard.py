31f0f8103245009911fcf4fc2d97824c71abcadd
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-06-16 20:58:24 +0900

    Update stockdashboard.py

--- stockdashboard.py
+++ stockdashboard.py
@@ -50,7 +50,7 @@ def main():
     
     # 라인 그래프 생성 with for문
     for i in range(len(stock)):
-        st.line_chart(fdr.DataReader(stock[i], start_date_str, end_date_str))
+        st.line_chart(fdr.DataReader(stock[i], start_date_str, end_date_str)['Close'])
         
     # create matplotlib line     
     for i in range(len(stock)):
