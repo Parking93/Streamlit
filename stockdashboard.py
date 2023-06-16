330b8556e55653e29f769f502c74cc89896b7251
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-06-16 23:54:34 +0900

    Update stockdashboard.py

--- stockdashboard.py
+++ stockdashboard.py
@@ -9,7 +9,7 @@ import streamlit as st
 import FinanceDataReader as fdr
 import matplotlib.pyplot as plt 
 import koreanize_matplotlib
-from datetime import datetime
+import datetime 
 
 
 def main():
@@ -29,7 +29,7 @@ def main():
     # 사용자로부터 시작 날짜와 종료 날짜 입력 받기
     col1, col2 = st.columns(2)
     with col1:
-        start_date = st.date_input('시작 날짜', datetime.date(2022,01,01))
+        start_date = st.date_input('시작 날짜', datetime.date(2022,1,1))
     with col2:
         end_date = st.date_input('종료 날짜')
    
