1a6f0ef4db3f736b1a3c31d7faff6f13146babba
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-06-19 18:36:56 +0900

    Update stockdashboard.py

--- stockdashboard.py
+++ stockdashboard.py
@@ -64,7 +64,7 @@ def main():
             plt.plot(fdr.DataReader(stock_list[i], start_date_str, end_date_str)['Close'])
         st.pyplot(plt)    
 
-    st.line_chart(fdr.DataReader('000660', '2022'))
+    st.line_chart(fdr.DataReader('000660', '2022')['Open','Close'])
 
 
 if __name__ == "__main__":
