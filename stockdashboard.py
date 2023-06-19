7f18e65093adcf9566fbe61154159beb775960d0
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-06-19 18:46:17 +0900

    Update stockdashboard.py

--- stockdashboard.py
+++ stockdashboard.py
@@ -64,8 +64,9 @@ def main():
     #         plt.plot(fdr.DataReader(stock_list[i], start_date_str, end_date_str)['Close'])
     #     st.pyplot(plt)    
 
-    df1 = fdr.DataReader(stock_list[0], start_date_str, end_date_str)['Close']
-    df2 = fdr.DataReader(stock_list[1], start_date_str, end_date_str)['Close']
+    st.write(stock_list)
+    # df1 = fdr.DataReader(stock_list[0], start_date_str, end_date_str)['Close']
+    # df2 = fdr.DataReader(stock_list[1], start_date_str, end_date_str)['Close']
     
     
 
