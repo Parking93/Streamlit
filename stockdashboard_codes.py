e69d6aad83f0ede300999aef7794764259c3f286
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-06-20 22:50:14 +0900

    Update stockdashboard_codes.py

--- stockdashboard_codes.py
+++ stockdashboard_codes.py
@@ -7,23 +7,6 @@
 
 
 
-import streamlit as st
-
-# 동영상 파일을 로컬 경로에서 재생하는 예시
-video_file = open('example.mov', 'rb')
-video_bytes = video_file.read()
-
-st.video(video_bytes)
-
-
-
-video_file = open('example.mp4', 'rb')
-video_bytes = video_file.read()
-
-st.video(video_bytes)
-
-
-
 
 
 
