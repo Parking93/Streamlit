d28cb5709b56901ca3c2d6e9028615dfbb596e52
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-06-20 22:51:01 +0900

    Update stockdashboard_codes.py

--- stockdashboard_codes.py
+++ stockdashboard_codes.py
@@ -3,6 +3,78 @@
 
 # In[ ]:
 
+import streamlit as st
+import FinanceDataReader as fdr
+import matplotlib.pyplot as plt 
+import koreanize_matplotlib
+import datetime 
+import numpy as np
+import pandas as pd
+
+
+def main():
+    # 제목
+    st.title("주식 차트 대시보드")
+
+    # 종목 선택 
+    stocks = ['삼성전자', 'SK하이닉스']
+    stock = st.multiselect('종목을 선택해주세요.', stocks) 
+    stock_dict = {'삼성전자':'005930', 'SK하이닉스':'000660'}
+    stock_list = []
+    for i in stock:
+        stock_list.append(stock_dict[i])
+    
+    
+    # 사용자로부터 시작 날짜와 종료 날짜 입력 받기
+    col1, col2 = st.columns(2)
+    with col1:
+        start_date = st.date_input('시작 날짜', datetime.date(2022,1,1))
+    with col2:
+        end_date = st.date_input('종료 날짜')
+   
+    # 날짜를 문자열로 변환
+    start_date_str = start_date.strftime('%Y-%m-%d')
+    end_date_str = end_date.strftime('%Y-%m-%d')
+
+
+    
+    # 매트릭 생성 
+    for i in range(len(stock_list)):
+        stock_value1 = fdr.DataReader(stock_list[i], start_date_str, end_date_str)["Close"].iloc[-1]
+        stock_value2 = fdr.DataReader(stock_list[i], start_date_str, end_date_str)["Close"].iloc[-2]
+        st.metric(label=f'{stock[i]}', value=f'{stock_value1}원', delta = f'{stock_value1 - stock_value2}원')
+                  
+
+
+
+    # Tab 생성 
+    tab1, tab2, tab3, tab4, tab5= st.tabs(['막대그래프' , '라인 그래프', 'matplotlib line chart', '겹쳐진 라인 그래프', '종가 막대그래프'])
+    with tab1:
+        st.subheader('📊막대 그래프')
+        for i in range(len(stock_list)):
+            st.subheader(f'{stock[i]}')
+            st.bar_chart(fdr.DataReader(stock_list[i], start_date_str, end_date_str)['Close'])
+    with tab2:
+        st.subheader('📈라인 그래프')
+        for i in range(len(stock_list)):
+            st.subheader(f'{stock[i]}')
+            st.line_chart(fdr.DataReader(stock_list[i], start_date_str, end_date_str)['Close'])
+    with tab3:
+        for i in range(len(stock_list)):
+            plt.plot(fdr.DataReader(stock_list[i], start_date_str, end_date_str)['Close'])
+        st.pyplot(plt)    
+    with tab4:
+        df1 = fdr.DataReader(stock_list[0], start_date_str, end_date_str)['Close']
+        df2 = fdr.DataReader(stock_list[1], start_date_str, end_date_str)['Close']
+        df3 = pd.merge(df1, df2, on='Date')
+        st.line_chart(df3)
+    with tab5:
+        
+        st.bar_chart(np.mean(df3, axis=0), x = ['삼성전자', '하이닉스'])
+
+        
+if __name__ == "__main__":
+    main()
 
 
 
