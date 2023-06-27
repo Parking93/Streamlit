fb9803ba793f4b6c543b9d0fa8d1616cdab24792
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-06-27 10:12:58 +0900

    Update stockdashboard.py

--- stockdashboard.py
+++ stockdashboard.py
@@ -19,7 +19,7 @@ def main():
     # 종목 선택 
     kospi_list = fdr.StockListing('KOSPI')
     
-    stocks = kospi_list['Code'].tolist()
+    stocks = kospi_list['Name'].tolist()
     stock = st.multiselect('종목을 선택해주세요.', stocks) 
     stock_dict = {'삼성전자':'005930', 'SK하이닉스':'000660'}
     stock_list = []
