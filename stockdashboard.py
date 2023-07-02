2de13c7311d92d45d22a9fb47b061e2ae178c807
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-07-02 21:15:19 +0900

    Update stockdashboard.py

--- stockdashboard.py
+++ stockdashboard.py
@@ -19,7 +19,7 @@ def main():
 
     # 주식 시장 종목 선택 
 
-    market = st.selectbox("주식시장을 선택하세요", ["KRX", "KOSPI", "KOSDAQ", "KONEX"])
+    market = st.sidebar.selectbox("주식시장을 선택하세요", ["KRX", "KOSPI", "KOSDAQ", "KONEX"])
     df = fdr.StockListing(market)
    
     
