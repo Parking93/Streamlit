5359b0c24af68e7e11059a5eaa96004835f46164
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-07-10 09:32:33 +0900

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -4,48 +4,48 @@
 # In[ ]:
 
 
-
 import streamlit as st
-import matplotlib.pyplot as plt
 import numpy as np
-st.write('---')
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import plotly.graph_objects as go
+
+# 예시 1 
+st.subheader('예시 1')
 
 arr = np.random.normal(1, 1, size=100)
+
 fig, ax = plt.subplots()
+
 ax.hist(arr, bins=20,color='skyblue', alpha=1)
 
 st.pyplot(fig)
 
 
-import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns 
-st.write('---')
+
+# 예시 2 
+
+st.subheader('예시 2')
 
 arr = np.random.normal(1, 1, size=100)
 
-fig = plt.figure(figsize=(10,4))
+fig, ax = plt.subplots()
 
 sns.histplot(arr, bins=20, color='pink', kde=True)
 
 st.pyplot(fig)
 
 
-import streamlit as st
-import plotly.graph_objects as go
-import numpy as np
 
-st.write('---')
+# 예시 3 
+
+st.subheader('예시 3')
 
 arr = np.random.normal(1, 1, size=100)
 
-fig = go.Figure(data=[go.Histogram(x=arr, nbinsx=10, marker_color='skyblue', opacity=0.7)])
+fig = go.Figure(data=[go.Histogram(x=arr, nbinsx=10, marker_color='green', opacity=0.7)])
 
 st.plotly_chart(fig)
-
-st.write('---')
-
 
 
 
