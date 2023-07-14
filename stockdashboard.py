1e2485cc78d4c01c615172d1bbb0ac4b2c09e785
jaehyeok.park <131845553+Parking93@users.noreply.github.com>
2023-07-14 10:35:45 +0900

    Update stockdashboard.py

--- stockdashboard.py
+++ stockdashboard.py
@@ -43,8 +43,8 @@ st.plotly_chart(fig)
 
 
 # 주식 종목 선택 
-list_kospi = fdr.StockListing('KOSPI')
-stocks = list_kospi['Name'].loc[:9].tolist()
+list_kospi = fdr.StockListing('KRX')
+stocks = list_kospi['Name'].tolist()
 stock = st.sidebar.multiselect('종목을 선택해주세요.', stocks) 
 
 list_stock = []
